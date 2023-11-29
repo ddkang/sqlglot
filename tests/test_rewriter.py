daed0407c596b937b8f4be2fbc1c1d97c942c2e1
ttt-77 <1057858192@qq.com>
2023-11-29 14:09:25 -0600

    support add_where and add_join in rewriter

--- tests/test_rewriter.py
+++ tests/test_rewriter.py
@@ -41,3 +41,64 @@ class TestRewriter(unittest.TestCase):
             .expression.sql("hive"),
             "SELECT *, a, SUM(b) AS c FROM (SELECT * FROM x) AS y",
         )
+
+
+    def test_add_where(self):
+        expression = parse_one("SELECT * FROM x WHERE col1 > 5")
+
+        self.assertEqual(
+            Rewriter(expression)
+                .add_where(
+                'AND',
+                'col2 IN (1, 2, 3)',
+            )
+                .expression.sql("hive"),
+            "SELECT * FROM x WHERE col2 IN (1, 2, 3) AND col1 > 5",
+        )
+
+        self.assertEqual(
+            Rewriter(expression)
+                .add_where(
+                'OR',
+                'col2 IN (1, 2, 3)',
+            )
+                .expression.sql("hive"),
+            "SELECT * FROM x WHERE col2 IN (1, 2, 3) OR col1 > 5",
+        )
+
+        expression = parse_one("SELECT * FROM x")
+
+        self.assertEqual(
+            Rewriter(expression)
+                .add_where(
+                '',
+                "(col1 IN (1, 2, 3) AND col2 = 3) OR col3 LIKE 'cat'",
+            )
+                .expression.sql("hive"),
+            "SELECT * FROM x WHERE (col1 IN (1, 2, 3) AND col2 = 3) OR col3 LIKE 'cat'",
+        )
+
+
+    def test_add_join(self):
+        expression = parse_one("SELECT * FROM x WHERE col1 > 5")
+
+        self.assertEqual(
+            Rewriter(expression)
+                .add_join(
+                'JOIN y ON x.col1 = y.col1 AND x.col2 = y.col2',
+            )
+                .expression.sql("hive"),
+            "SELECT * FROM x JOIN y ON x.col1 = y.col1 AND x.col2 = y.col2 WHERE col1 > 5",
+        )
+
+        expression = parse_one("SELECT * FROM x JOIN z ON x.col1 = z.col1 WHERE col1 > 5")
+
+        self.assertEqual(
+            Rewriter(expression)
+                .add_join(
+                'JOIN y ON objects00(x.col1) = objects00(y.col1) JOIN q ON color(x.col2) = color(q.col2)',
+            )
+                .expression.sql("hive"),
+            "SELECT * FROM x JOIN z ON x.col1 = z.col1 JOIN y ON objects00(x.col1) = objects00(y.col1) "
+            "JOIN q ON color(x.col2) = color(q.col2) WHERE col1 > 5",
+        )
